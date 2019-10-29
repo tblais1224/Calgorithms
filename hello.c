413fe6963e7dd93717e09dd4cd83aa47c89895bb
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-29 13:16:00 -0400

    practicing c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("Hello, people of Earth. I have come to take over your worthless lives.\n");
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 3)
+        {
+            printf("I'm on 3!\n");
+        }
+        printf("%i\n", i);
+    }
+}
