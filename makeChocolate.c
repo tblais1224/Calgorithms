42e28f6ce3f5bd8e9267c2a33d4ecd54a09d6528
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-14 12:41:14 -0500

    finished make chocolate

---
+++ makeChocolate.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int numberSmallBars(int smallBars, int bigBars, int kiloGoal)
+{
+    kiloGoal = bigBars * 5 > kiloGoal ? kiloGoal % 5 : kiloGoal - bigBars * 5;
+    return kiloGoal > 0 && kiloGoal > smallBars ? -1 : kiloGoal;
+}
+
+int main(void)
+{
+    int smallBars = get_int("How many small bars: ");
+    int bigBars = get_int("How many big bars: ");
+    int kiloGoal = get_int("Kilo goal: ");
+    printf("%i\n", numberSmallBars(smallBars, bigBars, kiloGoal));
+}
